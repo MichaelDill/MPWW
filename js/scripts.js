f4f3267ab6a4643513072cc3d168cf124c9ca407
mikedpicks <mddesignstl@gmail.com>
2017-09-09 13:48:12 -0500

    Add styles for section 1 and contact form

--- js/scripts.js
+++ js/scripts.js
@@ -8,11 +8,16 @@
 	// fade in .navbar
 	$(function () {
 		$(window).scroll(function () {
-            // set distance user needs to scroll before we fadeIn navbar
+						// set distance user needs to scroll before we fadeIn navbar
+			var navHt = $('.navbar').height();
+			var mgHt = $('.quote-border').css('margin-top');
+			
 			if ($(this).scrollTop() > $('.header').height()) {
 				$('.navbar-cont').addClass("fixed-top");
+				$('.quote-border').css('margin-top', mgHt + navHt);
 			} else {
 				$('.navbar-cont').removeClass("fixed-top");
+				$('.quote-border').css('margin-top', mgHt - navHt);
 			}
 		});
 
